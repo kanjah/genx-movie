d615fa1ec4233be56b6a8b656e8a4e5a2b1436c6
kanjah <patokanjah@gmail.com>
2024-01-02 11:48:06 +0300

    search page done

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,3 +1,5 @@
+import MoviesCarousel from "@/components/MoviesCarousel"
+import { getPopularMovies, getSearchedMovies } from "@/lib/getMovies"
 import { notFound } from "next/navigation"
 
 type Props = {
@@ -6,19 +8,31 @@ type Props = {
   }
 }
 
-function SearchPage({params: {term}}: Props) {
+async function SearchPage({params: {term}}: Props) {
   // console.log(props)
   if(!term) notFound()
 
   // remove white space from term
   const termToUse = decodeURI(term);
 
-  // API call to get searched movies
-  
+  // API call to get searched & popular movies
+    const movies = await getSearchedMovies(termToUse);
+  const popularMovies = await getPopularMovies();
 
   return (
-    <div>Welcome to the Search page: {termToUse}</div>
-  )
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {termToUse}</h1>
+        {/* <AISuggestion term={termToUse} /> */}
+
+        {/* verticle display */}
+        <MoviesCarousel title="Movies" movies={movies} isVertical />
+
+        {/* movies that you may like */}
+        <MoviesCarousel title="You may also like" movies={popularMovies} />
+      </div>
+    </div>
+  );
 }
 
-export default SearchPage
+export default SearchPage;
--- components/CarouselBanner.tsx
+++ components/CarouselBanner.tsx
@@ -21,15 +21,17 @@ function CarouselBanner({ movies }: { movies: Movie[] }) {
     >
       <div className="flex">
         {movies.map((movie) => (
+            // add flex{0 0 100%} to tailwind.config.js for this
           <div key={movie.id} className="flex-full min-w-0 relative">
             <Image
               key={movie.id}
+            //   full width
               src={getImagePath(movie.backdrop_path, true)}
               alt=""
               width={1920}
               height={1080}
             />
-
+                {/* hidden on mobile, "hidden" */}
             <div className="hidden lg:inline absolute mt-0 top-0 pt-40 xl:pt-52 left-0 lg:mt-40 bg-transparent z-20 h-full w-full bg-gradient-to-r from-gray-900/90 via-transparent to-transparent p-10 space-y-5 text-white ">
               <h2 className="text-5xl font-bold max-w-xl z-50">
                 {movie.title}
@@ -39,7 +41,7 @@ function CarouselBanner({ movies }: { movies: Movie[] }) {
           </div>
         ))}
       </div>
-
+            {/* bottom finishing for the pics on big carousel */}
       <div className="absolute inset-0 bg-gradient-to-b from-gray-200/0 via-gray-900/25 to-gray-300 dark:to-[#1A1C29]" />
     </div>
   );
