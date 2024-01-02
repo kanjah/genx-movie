447a8b7c54e08de5c9d6b0ae8e2289e3a0d00244
kanjah <patokanjah@gmail.com>
2024-01-02 11:55:41 +0300

    done search page

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,17 +1,32 @@
-type Props = {
-    params: {
-        id: string
-    },
-    searchParams: {
-        genre: string
-    }
-}
+// import AIAzureSuggestion from "@/components/AIAzureSuggestion";
+// import AISuggestion from "@/components/AISuggestion";
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getDiscoverMovies } from "@/lib/getMovies";
 
-function GenrePage({params: {id}, searchParams: {genre}}: Props) {
-    //console.log(props)
+async function GenrePage({
+  params: { id },
+  searchParams: { genre },
+}: {
+  params: { id: string };
+  searchParams: {
+    genre: string;
+  };
+}) {
+  console.log(id);
+  const movies = await getDiscoverMovies(id);
+
   return (
-    <div> Welcom to the genre with ID: {id} and name: {genre}</div>
-  )
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {genre}</h1>
+
+        {/* Azure OPENAI service suggestion */}
+        {/* <AIAzureSuggestion term={genre} /> */}
+        {/* <AISuggestion term={genre} /> */}
+        <MoviesCarousel title={`Genre`} movies={movies} isVertical />
+      </div>
+    </div>
+  );
 }
 
-export default GenrePage
+export default GenrePage;
